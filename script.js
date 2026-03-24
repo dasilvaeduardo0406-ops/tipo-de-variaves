9041ba1cc28fd34472158c59d09e65b5309c8ad9
dasilvaeduardo0406-ops <da.silva.eduardo0406@escola.pr.gov.br>
2026-03-24 15:23:28 +0000

    variavel

---
+++ script.js
@@ -0,0 +1,14 @@
+   // var
+
+    var altura =5
+    var comprimento = 7 ;
+
+    area = altura * comprimento;
+
+    console.log(area)
+
+   // let
+
+   let forma = ´retângulo`;
+   let altura = 7; 
+   
